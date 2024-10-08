a3aa8f17a0570007a8562ab05a01074a219a9cb7
ankit buade <ankitbuade20338@acropolis.in>
2024-10-08 12:03:08 +0530

    Practiced git post pust and other Api's

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 const express =require ("express")
 const app = express()
 
-// app.use((req,res)=>{
-//     res.send("hello world")
-// })
-
-app.use("/login",(req,res)=>{
-    res.send("hello this is login page")
+app.get("/user",(req,res)=>{
+    res.send("GET Api run successfully ")
 })
 
-app.use("/test",(req,res)=>{
-    res.send("hello this is test page ")
+app.post("/user",(req,res)=>{
+    res.send("POST Api run successfully")
 })
 
-app.use("/admin",(req,res)=>{
-    res.send("hello this is a admin page")
+app.put("/user", (req,res)=>{
+    res.send("PUT Api run successfully")
 })
+
+app.delete("/user",(req,res)=>{
+    res.send ("DELETE Api run successfully")
+})
+
 app.use("/user",(req,res)=>{
     res.send("hello this is use page")
 })
