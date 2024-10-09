ac0cfc09539e83a558800e915ec14ff84923ac53
ankit buade <ankitbuade20338@acropolis.in>
2024-10-09 11:59:05 +0530

    Wildcard Error handling

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ const {adminAuth} = require("./middlewares/auth")
 app.use("/admin", adminAuth)
 
 app.get("/admin/isUserDeleted",(req,res)=>{
+    throw new error("request not valid")
     res.send("user Deleted")
+})
+
+app.use("/" ,( err, req,res,next)=>{
+    if(err){
+        res.status(500).send("oops something went wrong")
+    }
 })
 
 app.listen(3000,()=>{
